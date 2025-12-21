c1c420f1cb6d8186cd9d68e03fb9a5e540e07937
Milton Munoz <livemilton@gmail.com>
2025-12-21 09:26:20 -0500

    React Zustand form :sparkles: Working with the Patient List

---
+++ src/components/PatientDetails.tsx
@@ -0,0 +1,14 @@
+import type { Patient } from "../types"
+
+
+type PatientDetailsProps = {
+    patient: Patient
+}
+
+export default function PatientDetails({patient}: PatientDetailsProps) {
+  return (
+    <div>
+        {patient.name}
+    </div>
+  )
+}
