401194d9862e2b1d54ed47a050706dc91c514b3f
Milton Munoz <livemilton@gmail.com>
2025-12-20 20:43:13 -0500

    React Zustand form :sparkles: Creating a component to display error messages and adding validation in form fields

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form"
+import Error from "./Error";
 
 export default function PatientForm() {
   
@@ -32,11 +33,21 @@ export default function PatientForm() {
                       type="text" 
                       placeholder="Nombre del Paciente" 
                       {...register('name', {
-                        required: 'El Nombre del paciente es obligatorio'
+                        required: 'El Nombre del paciente es obligatorio',
+                        maxLength: {
+                          value: 8,
+                          message: 'Máximo 8 Caracteres'
+                        }
                       })}
                   />
+                  {errors.name && (
+                    <Error>{errors.name?.message?.toString()}</Error>
+                  )}
 
-                  {errors.name?.message}
+                  {errors.maxLength && (
+                    <Error>{errors.maxLength?.message?.toString()}</Error>
+                  )}
+
               </div>
 
               <div className="mb-5">
@@ -48,7 +59,14 @@ export default function PatientForm() {
                     className="w-full p-3  border border-gray-100"  
                     type="text" 
                     placeholder="Nombre del Propietario" 
+                     {...register('caretaker', {
+                        required: 'El Propietario es obligatorio',
+                      })}
                 />
+
+                 {errors.caretaker && (
+                    <Error>{errors.caretaker?.message?.toString()}</Error>
+                  )}
               </div>
 
             <div className="mb-5">
@@ -60,7 +78,19 @@ export default function PatientForm() {
                   className="w-full p-3  border border-gray-100"  
                   type="email" 
                   placeholder="Email de Registro" 
+                  {...register("email", {
+                  required: "El Email es Obligatorio",
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: 'Email No Válido'
+                    }
+                })} 
+                  
               />
+
+               {errors.email && (
+                    <Error>{errors.email?.message?.toString()}</Error>
+                  )}
             </div>
 
             <div className="mb-5">
@@ -71,7 +101,14 @@ export default function PatientForm() {
                     id="date"
                     className="w-full p-3  border border-gray-100"  
                     type="date" 
-                />
+                    {...register('date', {
+                            required: 'La fecha de alta es obligatoria',
+                          })}
+                    />
+
+                    {errors.date && (
+                        <Error>{errors.date?.message?.toString()}</Error>
+                      )}
             </div>
             
             <div className="mb-5">
@@ -82,7 +119,15 @@ export default function PatientForm() {
                     id="symptoms"
                     className="w-full p-3  border border-gray-100"  
                     placeholder="Síntomas del paciente" 
-                ></textarea>
+                      {...register('symptoms', {
+                            required: 'Los síntomas son obligatorios',
+                          })}
+                   
+                />
+               
+                    {errors.symptoms && (
+                        <Error>{errors.symptoms?.message?.toString()}</Error>
+                      )}
             </div>
 
             <input
